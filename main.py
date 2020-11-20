19e680220b1e43ee7a6d75c7adf37947c4280503
novemberrain123 <pleaseworkusob@gmail.com>
2020-11-20 17:38:38 +0800

    added py file

---
+++ main.py
@@ -0,0 +1,35 @@
+from time import *
+from pyrebase import pyrebase
+
+config1 = {
+    "apiKey": "AIzaSyAH0JTJqYZaKiO-GssnbO9lIW_Z9-HMu0c",
+    "authDomain": "smart-toilet-adc07.firebaseapp.com",
+    "databaseURL": "https://smart-toilet-adc07.firebaseio.com/",
+    "storageBucket": "smart-toilet-adc07.appspot.com"
+}
+
+firebase1 = pyrebase.initialize_app(config1)
+auth1 = firebase1.auth()
+user1 = auth1.sign_in_with_email_and_password("pleaseworkusob@gmail.com", "Aa123456")
+db1 = firebase1.database()
+
+config2 = {
+    "apiKey": "AIzaSyAs_1NVtsjZ-LmTATAp0a0R5fK6XdKHaMU",
+    "authDomain": "bait2123-202010-03.firebaseapp.com",
+    "databaseURL": "https://bait2123-202010-03.firebaseio.com/",
+    "storageBucket": "bait2123-202010-03.appspot.com"
+}
+
+firebase2 = pyrebase.initialize_app(config2)
+auth2 = firebase2.auth()
+user2 = auth2.sign_in_with_email_and_password("bait2123.iot.03@gmail.com", "BeyondEducationH03")
+db2 = firebase2.database()
+x=1
+while x==1:
+    try: 
+        date = strftime("%Y%m%d", localtime())
+        #ultResults =  str(db2.child("PI_03_"+date).order_by_key().limit_to_last(1).order_by_key().limit_to_last(1).get().val())
+        #print(ultResults)
+        x=2
+    except KeyboardInterrupt: 
+        exit
