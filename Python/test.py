f42c6e3ae81267f9c41b1dacb8d31c5fc3d328d0
Lim Jun Shen <lengzai_roxes@hotmail.com>
2020-11-26 15:06:42 +0800

    update

--- Python/test.py
+++ Python/test.py
@@ -13,7 +13,7 @@ firebase1 = pyrebase.initialize_app(config1)
 auth1 = firebase1.auth()
 user1 = auth1.sign_in_with_email_and_password("pleaseworkusob@gmail.com", "Aa123456")
 db1 = firebase1.database()
-
+storage1 = firebase1.storage()
 config2 = {
     "apiKey": "AIzaSyAs_1NVtsjZ-LmTATAp0a0R5fK6XdKHaMU",
     "authDomain": "bait2123-202010-03.firebaseapp.com",
@@ -25,22 +25,23 @@ firebase2 = pyrebase.initialize_app(config2)
 auth2 = firebase2.auth()
 user2 = auth2.sign_in_with_email_and_password("bait2123.iot.03@gmail.com", "BeyondEducationH03")
 db2 = firebase2.database()
-
-
+storage2 = firebase2.storage()
+c = 0
+def takePic():
+    db2.child("PI_03_CONTROL").update({"camera":str(1)})
+    print(str(datetime.now()))
+    scd = str(datetime.now())[-9:]
+    sleep(10)
+    print(str(datetime.now()))
+    db2.child("PI_03_CONTROL").update({"camera":str(0)})
+    return scd
 while True:
     try:
-        Hour = str(datetime.now())[11:13]
-        date = strftime("%Y%m%d", localtime()) 
-
-        #Get the last child for the last sensor record which is Minute+Second, which is y
-        
-        y = db2.child("PI_03_" + date).child(Hour).order_by_key().limit_to_last(1).get()
-        for keyValue in y:
-            x = str(keyValue.key())
-            ultResults = db2.child("PI_03_" + date).child(Hour).child(x).child("ultra").get()
-            print(ultResults.val())
-            print("The path is > PI_03_"+date+" > "+Hour+" > "+x)
-
+        takePic()
+        ImgUrl = storage2.child("PI_03_CONTROL/cam_20201125123350.jpg").get_url(user1['idToken'])
+        ImgUrl1 = storage2.child("PI_03_CONTROL/cam_2020112512335.jpg").get_url(user1['idToken'])
+        print(len(ImgUrl))
+        print(len(ImgUrl1))
         break
     except KeyboardInterrupt: 
         exit
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import *
 from pyrebase import pyrebase
 from datetime import *
+import urllib.request, json
 
 config1 = {
     "apiKey": "AIzaSyAH0JTJqYZaKiO-GssnbO9lIW_Z9-HMu0c",
@@ -33,39 +34,50 @@ def takePic():
     sleep(10)
     print(str(datetime.now()))
     db2.child("PI_03_CONTROL").update({"camera":str(0)})
+
+def updUltsensor(ultName):
+    hour = str(datetime.now())[11:13]
+    date = strftime("%Y%m%d", localtime()) 
+
+    #Get the last child for the last sensor record which is Minute+Second, which is y
+
+    y = db2.child("PI_03_" + date).child(hour).order_by_key().limit_to_last(1).get()
+    for keyValue in y:
+        x = str(keyValue.key())
+        ultResults = db2.child("PI_03_" + date).child(hour).child(x).child("ultra").get().val() #record every 10 secs
+        db1.child("main").child(c).update({ultName:str(ultResults)}) #write ultrasensor results to db1
+        return int(ultResults)
+        
 while True:
     try:
-        Hour = str(datetime.now())[11:13]
-        date = strftime("%Y%m%d", localtime()) 
-
-        #Get the last child for the last sensor record which is Minute+Second, which is y
-        
-        y = db2.child("PI_03_" + date).child(Hour).order_by_key().limit_to_last(1).get()
-        for keyValue in y:
-            x = str(keyValue.key())
-            ultResults = db2.child("PI_03_" + date).child(Hour).child(x).child("ultra").get().val()
-            print(ultResults)
-            print("The path is > PI_03_"+date+" > "+Hour+" > "+x)
-        #record every 10 secs
-        db1.child("main").child(c).update({"ultra":str(ultResults)}) #write ultrasensor results to db1
         data = {
             "relay1":str(1), #red light & white light == on, blue light & green light == off
             "relay2":str(1),
         }
-        data1 = {
-            "relay1":str(0),
-            "relay2":str(0),
-        }
-        ultResults = 1 # test
-        if (ultResults<=20): #if user detected turn relay1 and relay2 on
-            db2.child("PI_03_CONTROL").update(data)
+        while True:
+            if (updUltsensor("ultra1")<=20): #if user detected turn relay1 and relay2 on
+                db2.child("PI_03_CONTROL").update(data)
+                break
+            sleep(0.5)
+
+        takePic() 
+
+        check = True
+        beginTime = perf_counter()
+        while check:
+            if (updUltsensor("ultra2")<=10):
+                beginTime = perf_counter() 
+                while check:
+                    if (updUltsensor("ultra2")>10):
+                        secondTime = perf_counter()
+                        while check:
+                            if(updUltsensor("ultra2")>10):
+                                if((perf_counter()-secondTime)>20):
+                                    check = False
+        overallTime = perf_counter() - beginTime
+        db1.child("main").child(c).update({"overallTime":str(overallTime)})
         
-        sleep(0.5)
 
-        takePic()
-
-        ImgUrl = storage2.child("PI_03_CONTROL/cam_2020112421311.jpg").download("E:\Code\smart-toilet","imga.jpg")
-        print(ImgUrl)
         break
     except KeyboardInterrupt: 
         exit
