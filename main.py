9cbcb4de9573ca5a999d1b012d5c085d52cb4b73
novemberrain123 <pleaseworkusob@gmail.com>
2020-11-28 03:43:42 +0800

    changes made

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ config1 = {
 
 firebase1 = pyrebase.initialize_app(config1)
 auth1 = firebase1.auth()
-user1 = auth1.sign_in_with_email_and_password("pleaseworkusob@gmail.com", "Aa123456")
+user1 = auth1.sign_in_with_email_and_password("pleaseworkusob@gmail.com",
+                                              "Aa123456")
 db1 = firebase1.database()
 storage1 = firebase1.storage()
 config2 = {
@@ -27,105 +28,149 @@ config2 = {
 
 firebase2 = pyrebase.initialize_app(config2)
 auth2 = firebase2.auth()
-user2 = auth2.sign_in_with_email_and_password("bait2123.iot.03@gmail.com", "BeyondEducationH03")
+user2 = auth2.sign_in_with_email_and_password("bait2123.iot.03@gmail.com",
+                                              "BeyondEducationH03")
 db2 = firebase2.database()
 storage2 = firebase2.storage()
 
-c = 0 #subfolder initialization
-r = sr.Recognizer()  
+c = 0  #subfolder initialization
+r = sr.Recognizer()
+
+
 def takePic():
-    db2.child("PI_03_CONTROL").update({"camera":str(1)})
+    db2.child("PI_03_CONTROL").update({"camera": str(1)})
     print(str(datetime.now()))
     sleep(10)
     print(str(datetime.now()))
-    db2.child("PI_03_CONTROL").update({"camera":str(0)})
+    db2.child("PI_03_CONTROL").update({"camera": str(0)})
 
-def updUltsensor(ultName):
+
+def getLatestSubfolder():
     hour = str(datetime.now())[11:13]
-    date = strftime("%Y%m%d", localtime()) 
+    date = strftime("%Y%m%d", localtime())
 
     #Get the last child for the last sensor record which is Minute+Second, which is y
 
-    y = db2.child("PI_03_" + date).child(hour).order_by_key().limit_to_last(1).get()
+    y = db2.child("PI_03_" +
+                  date).child(hour).order_by_key().limit_to_last(1).get()
+    return y, hour, date
+
+
+def updUltsensor(ultName):
+    y, hour, date = getLatestSubfolder()
     for keyValue in y:
         x = str(keyValue.key())
-        ultResults = db2.child("PI_03_" + date).child(hour).child(x).child("rand1").get().val() #record every 10 secs
-        ultResults2 = db2.child("PI_03_" + date).child(hour).child(x).child("rand2").get().val() #record every 10 secs
-        ultResults = min(ultResults,ultResults2)
-        db1.child("main").child(c).update({ultName:str(ultResults)}) #write ultrasensor results to db1
+        ultResults = db2.child("PI_03_" + date).child(hour).child(x).child(
+            "rand1").get().val()  #record every 10 secs
+        ultResults2 = db2.child("PI_03_" + date).child(hour).child(x).child(
+            "rand2").get().val()  #record every 10 secs
+        ultResults = min(ultResults, ultResults2)
+        db1.child("main").child(c).update(
+            {ultName: str(ultResults)})  #write ultrasensor results to db1
         return float(ultResults)
 
+
+def updSensor(input, output):
+    y, hour, date = getLatestSubfolder()
+    for keyValue in y:
+        x = str(keyValue.key())
+        results = db2.child("PI_03_" + date).child(hour).child(x).child(
+            input).get().val()
+        db1.child("main").child(c).update({output: str(results)})
+
+
 def speechToText():
-    
-     while(1):     
-        
-        # Exception handling to handle 
-        # exceptions at the runtime 
-        try: 
+    wash_v = ["wash", "war", "lasts",
+                  "watch"]  #list of words meant to mean wash
+    flush_v = ["flush","flash","lush","slush","flourish"]
+    while True:
+        try:
+            with sr.Microphone() as source2:
+
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+
+                #listens for the user's input
+                audio2 = r.listen(source2)
+
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
+
+                if (MyText.split(' ')[0] in (wash_v or flush_v) and MyText.split(' ')[3].isdigit()):
+                    print(MyText)
+                    return MyText
+                else:
+                    print("Please repeat your command")
             
-            # use the microphone as source for input. 
-            with sr.Microphone() as source2: 
-                
-                # wait for a second to let the recognizer 
-                # adjust the energy threshold based on 
-                # the surrounding noise level  
-                r.adjust_for_ambient_noise(source2, duration=0.2) 
-                
-                #listens for the user's input  
-                audio2 = r.listen(source2) 
-                
-                # Using ggogle to recognize audio 
-                MyText = r.recognize_google(audio2) 
-                MyText = MyText.lower() 
-    
-                print("Did you say "+MyText) 
-                SpeakText(MyText) 
-                
-        except sr.RequestError as e: 
-            print("Could not request results; {0}".format(e)) 
-            
-        except sr.UnknownValueError: 
-            print("unknown error occured")   
+
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+        except sr.UnknownValueError:
+            print("Please repeat")
+
+
 while True:
     try:
         data = {
-            "relay1":str(1), #red light & white light == on, blue light & green light == off
-            "relay2":str(1),
+            "relay1": str(
+                1
+            ),  #red light & white light == on, blue light & green light == off
+            "relay2": str(1),
         }
         while True:
-            if (1<=20): #if user detected turn relay1 and relay2 on correct: updUltsensor("ultra1")
+            if (
+                    1 <= 20
+            ):  #if user detected turn relay1 and relay2 on correct: updUltsensor("ultra1")
                 db2.child("PI_03_CONTROL").update(data)
                 break
             sleep(9)
 
-        #takePic() 
+        #takePic()
 
         check = True
         beginTime = perf_counter()
         while check:
-            if (updUltsensor("ultra2")<=20):
-                beginTime = perf_counter() 
+            if (updUltsensor("ultra2") <= 20):
+                beginTime = perf_counter()
                 while check:
-                    if (updUltsensor("ultra2")>20):
+                    if (updUltsensor("ultra2") > 20):
                         secondTime = perf_counter()
                         while check:
-                            if(updUltsensor("ultra2")>20):
-                                if((perf_counter()-secondTime)>20):
+                            if (updUltsensor("ultra2") > 20):
+                                if ((perf_counter() - secondTime) > 20):
                                     check = False
-                            else: 
+                            else:
                                 break
         overallTime = perf_counter() - beginTime
-        db1.child("main").child(c).update({"overallTime":str(overallTime)}) #update time to main db
+        db1.child("main").child(c).update({"overallTime": str(overallTime)
+                                           })  #update time to main db
 
         #random image is chosen and displayed
-        bin = ["pee_clear","pee_yellow","pee_pink","poo_black","poo_brown","poo_yellow"]
+        bin = [
+            "pee_clear", "pee_yellow", "pee_pink", "poo_black", "poo_brown",
+            "poo_yellow"
+        ]
         randS = "E:\Code\smart-toilet\img\\" + random.choice(bin) + ".png"
         storage2.child("image/oled.jpg").put(randS)
-        db2.child("PI_03_CONTROL").update({"oledsc":"1"})
+        db2.child("PI_03_CONTROL").update({"oledsc": "1"})
 
+        #picture taken
         takePic()
 
         spokenCommand = speechToText()
+        wash_v = ["wash", "war", "lasts",
+                  "watch"]  #list of words meant to mean wash
+        if (spokenCommand.split(' ')[0] in wash_v):
+            washTime = int(spokenCommand.split(' ')[2])
+            db2.child("PI_03_CONTROL").update({"buzzer": "1"})
+            timeout = time() + washTime #loop time = washTime
+            while True:
+                updSensor("sound","sound")
+                if(time()>timeout):
+                    break
+            
+            else:
+
         break
-    except KeyboardInterrupt: 
+    except KeyboardInterrupt:
         exit
