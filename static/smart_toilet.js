078f36aad5cc00fc3c96d0ab2907b7149b33c1ba
unknown <ur email>
2020-12-02 13:29:11 +0800

    new features

--- static/smart_toilet.js
+++ static/smart_toilet.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $("#image").hide();
     // display the charts
     $("#chartContainer").attr("style", "display:block");
+
   });
 });
 
@@ -13,6 +14,28 @@ $(document).ready(function () {
     $("#image").hide();
     // display the charts
     $("#chartContainer").attr("style", "display:block");
+
+  });
+});
+
+$(document).ready(function () {
+  $(document).on("click", "#light", function () {
+    // hide main and results pages + hide input form
+    $("#image").hide();
+    // display the charts
+    $("#chartContainer").attr("style", "display:block");
+
+  });
+});
+
+$(document).ready(function () {
+  $(document).on("click", "#sound", function () {
+    // hide main and results pages + hide input form
+    $("#image").hide();
+    // display the charts
+    $("#chartContainer").attr("style", "display:block");
+    
+
   });
 });
 
@@ -24,6 +47,24 @@ $(document).ready(function () {
     $("#picture").attr("style", "display:block");
   });
 });
+
+$(document).ready(function () {
+  $(document).on("click", "#home", function () {
+    // hide main and results pages + hide input form
+    $("#picture").hide();
+    $("#chartContainer").hide();
+    // display the charts
+    // $("#graph").attr("style", "display:inline");
+    $("#image").attr("style", "display:block");
+    $("#image").attr("border","11","solid");
+    $("#image").css("margin-left","-100px");
+
+  });
+});
+
+function eraseText() {
+  document.getElementById("console").value = "";
+}
 
 var config = {
   apiKey: "AIzaSyAH0JTJqYZaKiO-GssnbO9lIW_Z9-HMu0c",
@@ -72,100 +113,117 @@ document.getElementById("retrieve").onclick = function () {
       console.log("Error occurred");
     });
 };
-//graph for ultrasonic 1
-document.getElementById("ult1").onclick = function () {
+
+
+// //graph for ultrasonic 1
+// document.getElementById("ult1").onclick = function () {
+//   var dps = []; // dataPoints
+//   var chart = new CanvasJS.Chart("chartContainer", {
+//     title: {
+//       text: "Ultrasonic sensor value",
+//     },
+//     data: [
+//       {
+//         type: "line",
+//         dataPoints: dps,
+//       },
+//     ],
+//   });
+
+//   var xVal = 0;
+//   var yVal = 0;
+//   var updateInterval = 1000;
+//   var dataLength = 20; // number of dataPoints visible at any point
+//   var path;
+//   var nextPath;
+//   var key;
+
+//   var updateChart = function (count) {
+//     count = count || 1;
+
+//     for (var j = 0; j < count; j++) {
+//       var date = new Date();
+//       var year = date.getFullYear().toString();
+//       var month = (date.getMonth() + 1).toString();
+//       var day = date.getDate().toString();
+//       var days;
+//       if (day < 10) {
+//         days = ("0" + day).toString();
+//       } else {
+//         days = day.toString();
+//       }
+
+//       var query = firebase
+//         .database()
+//         .ref("main/" + year + month + days)
+//         .orderByKey();
+//       query.once("value").then(function (snapshot) {
+//         for (var x in snapshot.val()) {
+//           key = x;
+//         }
+//         path = parseInt(key);
+
+//         if (path >= 1000) {
+//           nextPath = toString(path);
+//         } else if (path >= 100) {
+//           nextPath = "0" + path.toString();
+//         } else if (path >= 10) {
+//           nextPath = "00" + path.toString();
+//         } else {
+//           nextPath = "000" + path.toString();
+//         }
+
+//         firebase
+//           .database()
+//           .ref("main/" + year + month + days + "/" + nextPath)
+//           .on("value", function (valSnapshot) {
+//             yVal = valSnapshot.val().ultra1;
+//           });
+//       });
+//       var yValue_1 = parseInt(yVal);
+
+//       dps.push({
+//         x: xVal,
+//         y: yValue_1,
+//       });
+
+//       xVal += 1;
+//     }
+
+//     if (dps.length > dataLength) {
+//       dps.shift();
+//     }
+
+//     chart.render();
+//   };
+
+//   updateChart(dataLength);
+//   setInterval(function () {
+//     updateChart();
+//   }, updateInterval);
+// };
+
+// //graph for ultrasonic2
+document.getElementById("ult1").onclick = function() 
+{graphDisplay("ultra1")};
+
+document.getElementById("ult2").onclick = function() 
+{graphDisplay("ultra2")};
+
+document.getElementById("sound").onclick = function() 
+{graphDisplay("sound")};
+
+document.getElementById("light").onclick = function() 
+{graphDisplay("light")};
+
+
+function graphDisplay(sensor_type) 
+{
+  var sensorName=sensor_type.toString();
   var dps = []; // dataPoints
   var chart = new CanvasJS.Chart("chartContainer", {
     title: {
-      text: "Ultrasonic sensor value",
-    },
-    data: [
-      {
-        type: "line",
-        dataPoints: dps,
-      },
-    ],
-  });
-
-  var xVal = 0;
-  var yVal = 0;
-  var updateInterval = 1000;
-  var dataLength = 20; // number of dataPoints visible at any point
-  var path;
-  var nextPath;
-  var key;
-
-  var updateChart = function (count) {
-    count = count || 1;
-
-    for (var j = 0; j < count; j++) {
-      var date = new Date();
-      var year = date.getFullYear().toString();
-      var month = (date.getMonth() + 1).toString();
-      var day = date.getDate().toString();
-      var days;
-      if (day < 10) {
-        days = ("0" + day).toString();
-      } else {
-        days = day.toString();
-      }
-
-      var query = firebase
-        .database()
-        .ref("main/" + year + month + days)
-        .orderByKey();
-      query.once("value").then(function (snapshot) {
-        for (var x in snapshot.val()) {
-          key = x;
-        }
-        path = parseInt(key);
-
-        if (path >= 1000) {
-          nextPath = toString(path);
-        } else if (path >= 100) {
-          nextPath = "0" + path.toString();
-        } else if (path >= 10) {
-          nextPath = "00" + path.toString();
-        } else {
-          nextPath = "000" + path.toString();
-        }
-
-        firebase
-          .database()
-          .ref("main/" + year + month + days + "/" + nextPath)
-          .on("value", function (valSnapshot) {
-            yVal = valSnapshot.val().ultra1;
-          });
-      });
-      var yValue_1 = parseInt(yVal);
-
-      dps.push({
-        x: xVal,
-        y: yValue_1,
-      });
-
-      xVal += 1;
-    }
-
-    if (dps.length > dataLength) {
-      dps.shift();
-    }
-
-    chart.render();
-  };
-
-  updateChart(dataLength);
-  setInterval(function () {
-    updateChart();
-  }, updateInterval);
-};
-
-//graph for ultrasonic2
-document.getElementById("ult2").onclick = function () {
-  var dps = []; // dataPoints
-  var chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-      text: "Ultrasonic sensor value",
+      text: sensorName+" sensor value",
     },
     data: [
       {
@@ -222,15 +280,15 @@ document.getElementById("ult2").onclick = function () {
           .database()
           .ref("main/" + year + month + days + "/" + nextPath)
           .on("value", function (valSnapshot) {
-            yVal = valSnapshot.val().ultra2;
+            yVal = valSnapshot.val().sensorName;
           });
       });
 
-      var yValue_2 = parseInt(yVal);
+      var yValue = parseInt(yVal);
 
       dps.push({
         x: xVal,
-        y: yValue_2,
+        y: yValue,
       });
 
       xVal += 1;
